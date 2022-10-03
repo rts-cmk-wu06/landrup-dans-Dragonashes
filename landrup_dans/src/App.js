85f46e663dc09b1459e15b5d969ef2b6238db895
Dragonashes <dlsfs@tutanota.de>
2022-10-04 01:26:07 +0200

    add assets

--- landrup_dans/src/App.js
+++ landrup_dans/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import splash from './assets/splash-image.jpg'
+import navicon1 from './assets/Group 1.svg'
+import navicon2 from './assets/Group 2.svg'
+import navicon3 from './assets/Group 3.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={splash} className="App-logo" alt="logo" />
+
+      <img src={navicon1} className="App-logo" alt="logo" />
+      <img src={navicon2} className="App-logo" alt="logo" />
+      <img src={navicon3} className="App-logo" alt="logo" />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
