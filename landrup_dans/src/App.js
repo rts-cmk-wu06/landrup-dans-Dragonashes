161a82f2c9254b1679b2a16b9f1b7a642f30dcd3
Dragonashes <dlsfs@tutanota.de>
2022-10-09 14:46:34 +0200

    add cleanup pass

--- landrup_dans/src/App.js
+++ landrup_dans/src/App.js
@@ -6,15 +6,8 @@ function App() {
     <div className="App">
 
       <div className='hero1'>
-        {/* <h1 className="hero1-h1">LANDRUP</h1>
-        <h2 className='hero1-h2'>
-          <span className='hero1-h2-span'>&nbsp; DANS</span>
-          <hr className='hero1-h2-hr'></hr>
-        </h2> */}
-
         <div className="placementgrid">
           <h1 className="hero1-h1">LANDRUP</h1>
-          {/* &nbsp; */}
           <span className='hero1-h2-span'>DANS</span>
           <hr className='hero1-h2-hr'></hr>
         </div>
